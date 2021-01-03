77485f057533768fa426d24d63485ada80ea70c0
JúDCraide <juliadcraide31@gmail.com>
2021-01-03 15:26:46 -0300

    Create and list transaction

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,28 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    const transactions = await this.find();
+
+    transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        income += Number(transaction.value);
+      } else {
+        outcome += Number(transaction.value);
+      }
+    });
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      total,
+      income,
+      outcome,
+    };
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,67 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const categoriesRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw new AppError('Sem dinheiro para realizar a transação', 400);
+      }
+    }
+
+    const categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+      await categoriesRepository.save(newCategory);
+
+      const transaction = transactionsRepository.create({
+        title,
+        value,
+        category_id: newCategory.id,
+        type,
+      });
+
+      await transactionsRepository.save(transaction);
+
+      return transaction;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      category_id: categoryExists.id,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
